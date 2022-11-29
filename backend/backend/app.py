8fcf0d1c01961f9a3bbf08a893cb09e845e4bf5c
Simon Woodward <simon@redcabbage.org>
2022-11-29 18:21:53 +0000

    Initial experiments with forms, logins, ...

--- backend/backend/app.py
+++ backend/backend/app.py
@@ -10,14 +10,14 @@ import structlog
 from .logging import init_logging, init_app_logging
 from . import models
 
-# from flask_login import (
-#     UserMixin,
-#     login_user,
-#     LoginManager,
-#     current_user,
-#     logout_user,
-#     login_required,
-# )
+from flask_login import (
+    UserMixin,
+    login_user,
+    LoginManager,
+    current_user,
+    logout_user,
+    login_required,
+)
 
 init_logging(config.Config.ENVIRONMENT == config.DEV)
 log = structlog.get_logger(source_module=__name__)
@@ -26,20 +26,26 @@ log = structlog.get_logger(source_module=__name__)
 # Singleton factory doodads
 migrate = Migrate()
 bcrypt = Bcrypt()
-# login_manager = LoginManager()
-# login_manager.session_protection = "strong"
-# login_manager.login_view = "login"
-# login_manager.login_message_category = "info"
+login_manager = LoginManager()
+login_manager.session_protection = "strong"
+login_manager.login_view = "login"
+login_manager.login_message_category = "info"
+
+@login_manager.user_loader
+def load_user(user_id):
+    return models.User.get(user_id)
 
 
 def create_app():
-    app = Flask(__name__)
+    app = Flask(__name__, static_url_path="")
     init_app_logging(app)
     app.config.from_object(config.Config)
 
-    # login_manager.init_app(app)
+    login_manager.init_app(app)
     db.init_app(app)
     migrate.init_app(app, db)
     bcrypt.init_app(app)
 
     return app
+
+app = create_app()
--- backend/backend/models.py
+++ backend/backend/models.py
@@ -4,8 +4,7 @@ from backend.db import db
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True, nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
+    username = db.Column(db.String(80), unique=True, nullable=False, index=True)
     pwd = db.Column(db.String(300), nullable=False, unique=True)
 
     def __repr__(self):
